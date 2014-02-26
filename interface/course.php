5b96811a57a4b76551b5c097fb8e127aea6a19e7
Tomas <tomsel1@hotmail.com>
2014-02-27 00:21:09 +0100

    made a class of database.php so we can invoke it from other classes

--- interface/course.php
+++ interface/course.php
@@ -3,17 +3,28 @@
     <head>
         <meta charset="utf-8" />
         <title>Stu Dent's Home Page + Date and Time</title>
+        
     </head>
     <body>
         <h1>E - blackBoard page 2</h1>
         <?php 
-		$lessons = 2; //hämta antal lektioner från databas
+
+        echo "funkar det + ";
+        include ('database.php');
+		echo "database.php is included ";
+        $database = new MyDb();
+        echo "an object MydB is created ";
+        $test = $database ->create_database();
+        echo $test;
+       // $test2 =$database ->add_data(); // can´t invoke method add_data() from database.php 
+        //echo $test2;
+        $lessons = 2; //hämta antal lektioner från databas
 		$lesson_date = '2014-02-25';
 		echo $_GET["course"]."</br>";
 		for($i=0; $i<$lessons; $i++){
 			echo '<a href="display.php">'.$lesson_date."</a></br>";
 		}
-			
+		
       	?>
     </body>
 </html>
--- interface/database.php
+++ interface/database.php
@@ -1,72 +1,76 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-    </head>
-    <body>
+
     <?php
       class MyDB extends SQLite3
       {
          function __construct()
          {
             $this->open('test.db');
-         }
-      }
-      $db = new MyDB();
-      if(!$db){
-         echo "this is wroooong\n";
-      } else {
-         echo "Opened database successfully\n";
-      }
 
-      $sql =<<<EOF
-        CREATE TABLE IF NOT EXISTS LECTURENOTES
-        (ID INTEGER PRIMARY KEY AUTOINCREMENT,
-        PATH            TEXT      NOT NULL,
-        DATE            TEXT  NOT NULL,
-        COURSE        CHAR(50));
+         }
+       
+         function create_database()
+         {
+            $db = new MyDB();
+            if(!$db){
+              echo "this is wroooong\n";
+            } else {
+               echo "Opened database successfully\n";
+            }
+
+            $sql =<<<EOF
+              CREATE TABLE IF NOT EXISTS LECTURENOTES
+              (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+              PATH            TEXT      NOT NULL,
+              DATE            TEXT  NOT NULL,
+              COURSE        CHAR(50));
 EOF;
 
-      $ret = $db->exec($sql);
-      if(!$ret){
-         echo "this is wroooong\n";
-      } else {
-         echo "Table created successfully\n";
-      }
+            $ret = $db->exec($sql);
+            if(!$ret){
+               echo "this is wroooong\n";
+            } else {
+               echo "Table created successfully\n";
+            }     
+          } 
+
+
+
+      
       //$db->close();
       
       //__insert("aaa", CURRENT_DATE, "TDA517")
-      //function __insert($path, $date, $course)
-      $dummyVariable = true;
-      if($dummyVariable) {  
-        $sql =<<<EOF
-            INSERT INTO LECTURENOTES (PATH,DATE,COURSE)
-            VALUES ('sss/sss', 'date', 'TDA517');
-EOF;
+      function add_data()
+      {
         
-        $ret = $db->exec($sql);
-        if(!$ret){
-          echo $db->lastErrorMsg() . " errormsg when inserting data";
-        } else {
-          echo "Records created successfully\n";
-        }
-      }
-      $sql =<<<EOF
-            SELECT * FROM LECTURENOTES;
+            $sql =<<<EOF
+              INSERT INTO LECTURENOTES (PATH,DATE,COURSE)
+              VALUES ('sss/sss', 'date', 'TDA517');
 EOF;
-      $ret = $db->query($sql);
-      while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
-        echo "ID = ". $row['ID'] . "\n";
-        echo "PATH = ". $row['PATH'] ."\n";
-        echo "DATE = ". $row['DATE'] ."\n";
-        echo "COURSE =  ".$row['COURSE'] ."\n\n";
-       } 
-      $db->close();
-    
+          
+          $ret = $db->exec($sql);
+          if(!$ret){
+            echo $db->lastErrorMsg() . " errormsg when inserting data";
+          } else {
+            echo "Records created successfully\n";
+          }
+        }
+      
+      function show_data()
+      {
+        $sql =<<<EOF
+              SELECT * FROM LECTURENOTES;
+EOF;
+        $ret = $db->query($sql);
+        while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
+          echo "ID = ". $row['ID'] . "\n";
+          echo "PATH = ". $row['PATH'] ."\n";
+          echo "DATE = ". $row['DATE'] ."\n";
+          echo "COURSE =  ".$row['COURSE'] ."\n\n";
+        } 
+        $db->close();
+      }
+    }
    ?>        
-   
-   </body>
-</html>
 
 
 
