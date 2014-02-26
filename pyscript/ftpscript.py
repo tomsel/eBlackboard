0b33e3ab09e5955dadd46fcb7ba0555780e75aec
pi28 <pi28@outlook.com>
2014-02-26 04:16:18 +0100

    Iteration 1 lite

--- pyscript/ftpscript.py
+++ pyscript/ftpscript.py
@@ -1,6 +1,6 @@
 import ftplib 
 def upload(filename,course):
-	session = ftplib.FTP('195.74.38.22/','193040_master','eblackboard') 
+	session = ftplib.FTP('195.74.38.22','193040_master','eblackboard') 
 	file = open(filename,'rb') # file to send
 	#att implementera: skapa mapp om den inte finns 
 	session.storbinary('STOR /eblackboard.se/public_html/img/'+course+'/'+filename, file) # send the file 
--- pyscript/sensor.py
+++ pyscript/sensor.py
@@ -1,15 +1,17 @@
+import subprocess
 import time
 import os
-import RPi.GPIO as GPIO
+import sys
+#import RPi.GPIO as GPIO
 import Image
 from ftpscript import upload
 from mysqlcon import insertdata
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(22, GPIO.IN)		#LILA
-GPIO.setup(17, GPIO.IN)		#BRUN
-GPIO.setup(24, GPIO.OUT)	#ROD
-GPIO.setup(25, GPIO.OUT)	#BLA
+#GPIO.setmode(GPIO.BCM)
+#GPIO.setup(22, GPIO.IN)		#LILA
+#GPIO.setup(17, GPIO.IN)		#BRUN
+#GPIO.setup(24, GPIO.OUT)	#ROD
+#GPIO.setup(25, GPIO.OUT)	#BLA
 
 tunnel=subprocess.Popen("python2.7 tunnel.py", shell=True)
 imgcounter = 0 #nollas aldrig i programmet
@@ -19,12 +21,12 @@ course='TDA514' #ska fungera automatiskt i framtiden
 
 try:
 	while True:
-		if GPIO.input(17) == False:
+		if raw_input("waiting for input"):
 			time.sleep(3)
-			GPIO.output(24, True)
+			#GPIO.output(24, True)
 			#ta bild
-			imgcounter++
-			filename=date+str(imgcounter)+'.jpg'
+			imgcounter+=1
+			filename=date+'.'+str(imgcounter)+'.jpg'
 			os.system('raspistill -o '+filename)
 			time.sleep(9)
 
@@ -37,18 +39,18 @@ try:
 			upload(filename,course)
 			#ladda hem ics och ta fram data
 			#pupulera databas
-			insertdata(imgpath+course+'/'+filename, date, course)
-		
-			while GPIO.input(17) == False:
-		       		count = 1
-	   		GPIO.output(24, False)
+			insertdata('../img/'+course+'/'+filename, date, course)
+			
+			#while GPIO.input(17) == False:
+		       	#	count = 1
+	   		#GPIO.output(24, False)
 
-	   	if GPIO.input(22) == False:
+	   	if False:
 			time.sleep(3) 
-			GPIO.output(25, True)
+			#GPIO.output(25, True)
 			#ta bild
-			imgcounter++
-			filename=date+str(imgcounter)+'.jpg'
+			imgcounter+=1
+			filename=date+'.'+str(imgcounter)+'.jpg'
 			os.system('raspistill -o '+filename)
 			time.sleep(9)
 			
@@ -63,10 +65,11 @@ try:
 			#pupulera databas
 			insertdata(imgpath+course+'/'+filename, date, course)
 		
-	    	while GPIO.input(22) == False:
-	    		count = 2
-			GPIO.output(25, False)  
+	    		#while GPIO.input(22) == False:
+	    		#count = 2
+			#GPIO.output(25, False)  
 			
-except KeyboardInterrupt
+except KeyboardInterrupt:
 	tunnel.terminate()
 	print("Process terminated")
+	sys.exit(0)
