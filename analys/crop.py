7c12fe2b1fcd8c6ebcc7e1b57e57e58e75bb2e18
DennisParviainen <loadzero@gmail.com>
2014-03-03 14:33:08 +0100

    updated the crop function

--- analys/crop.py
+++ analys/crop.py
@@ -1,12 +1,16 @@
 import Image
 
-def crop_image(image, finishedimage, x, y, width, height):
-    input_img = Image.open(image)
-    box = (x, y, x + width, y + height)
-    output_img = input_img.crop(box)
-    output_img.save(finishedimage +".png")
+#def crop_image(image, finishedimage, x, y, width, height):
+#   input_img = Image.open(image)
+#    box = (x, y, x + width, y + height)
+#    output_img = input_img.crop(box)
+#    output_img.save(finishedimage +".png")
 
-def main():
-    crop_image("image.png","output", 0, 0, 1029, 300)
-
-if __name__ == '__main__': main()
+input_img=Image.open("image.jpg")
+box= (300, 300, 1300, 1000)
+output_img = input_img.crop(box)
+output_img.save("croppedimg.jpg")
+#def main():
+#    crop_image("image.jpg","output", 0, 0, 1029, 300)
+#
+#if __name__ == '__main__': main()
