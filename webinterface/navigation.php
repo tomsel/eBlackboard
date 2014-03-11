747cfc3c766cd4aba1371a68f81fc6d9fffd140b
Tomas <tomsel1@hotmail.com>
2014-03-11 11:36:10 +0100

    changed directory structure

--- webinterface/navigation.php
+++ webinterface/navigation.php
@@ -4,10 +4,10 @@
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-         <script type="text/javascript" src="/eBlackboard/webinterface/library/jquery-1.11.0.js"></script>
+         <script type="text/javascript" src="/library/jquery-1.11.0.js"></script>
         <!-- Bootstrap -->
-        <link href="/eBlackboard/webinterface/library/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-        <script type="text/javascript" src="/eBlackboard/webinterface/library/bootstrap/js/bootstrap.min.js"></script>
+        <link href="/library/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <script type="text/javascript" src="/library/bootstrap/js/bootstrap.min.js"></script>
     </head>
 
     <body>
