a6fa9fd895e5c221fdf3be114e08f61bd3823d99
Simon Andersson <c.simon.andersson@gmail.com>
2014-03-11 11:26:58 +0100

    fixing paths

--- pyscript/ftpscript.py
+++ pyscript/ftpscript.py
@@ -3,7 +3,7 @@ def upload(filename,course):
 	session = ftplib.FTP('195.74.38.22/','193040_master','eblackboard') 
 	file = open(filename,'rb') # file to send
 	#att implementera: skapa mapp om den inte finns 
-	session.storbinary('STOR /eblackboard.se/public_html/eBlackboard/webinterface/img/'+course+'/'+filename, file) # send the file 
+	session.storbinary('STOR /eblackboard.se/public_html/img/'+course+'/'+filename, file) # send the file 
 	file.close() # close file 
 	session.quit #close FTP
 
