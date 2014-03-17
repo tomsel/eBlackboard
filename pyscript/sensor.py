a0b60398c6935c5920cde105978efcd8b096c347
pi28 <pi28@outlook.com>
2014-03-17 17:22:02 +0100

    working on it[1/2]

--- pyscript/sensor.py
+++ pyscript/sensor.py
@@ -3,18 +3,18 @@ import signal
 import time
 import os
 import sys
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import Image
 from datetime import date
 from ftpscript import upload
 from mysqlcon import insertdata
 from download import get_course
 
-#GPIO.setmode(GPIO.BCM)
-#GPIO.setup(22, GPIO.IN)		#LILA
-#GPIO.setup(17, GPIO.IN)		#BRUN
-#GPIO.setup(24, GPIO.OUT)	#ROD
-#GPIO.setup(25, GPIO.OUT)	#BLA
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(22, GPIO.IN)		#LILA
+GPIO.setup(23, GPIO.IN)		#BRUN
+GPIO.setup(24, GPIO.OUT)	#ROD
+GPIO.setup(25, GPIO.OUT)	#BLA
 
 tunnel=subprocess.Popen("python2.7 tunnel.py", shell=True, preexec_fn=os.setsid)
 datestamp = date.today().isoformat()
@@ -28,19 +28,18 @@ os.system("rm IP.txt")
 try:
 	while True:
 		if raw_input("waiting for input"):
-			time.sleep(3)
+			#time.sleep(3)
 			#GPIO.output(24, True)
 			#ta bild
 			timestamp = time.strftime("%H:%M:%S", time.gmtime())
-			filename="logo.png"
-			#filename=datestamp+'.'+timestamp+'.jpg'
-			#os.system('raspistill -o '+filename)
-			time.sleep(9)
+			filename=datestamp+'.'+timestamp+'.jpg'
+			os.system('raspistill -o '+'image.jpg')
+			#time.sleep(9)
 
-			#input_img=Image.open("image.jpg")
-			#box= (300, 300, 1300, 1000)
-			#output_img = input_img.crop(box)
-			#output_img.save("croppedimg.jpg")
+			input_img=Image.open("image.jpg")
+			box= (300, 300, 1300, 1000)
+			output_img = input_img.crop(box)
+			output_img.save(filename)
 			
 			#ladda upp bild till ftp
 			upload(filename,course)
