4d8f1796a854c4e2019092844b05ce0c2f96d5ff
DennisParviainen <loadzero@gmail.com>
2014-04-23 14:53:04 +0200

    added improvements to the crop functions

--- pyscript/cropmain.py
+++ pyscript/cropmain.py
@@ -1,11 +1,12 @@
 
-
+#from main import *
 from SimpleCV import *
 from crophelp import *
 import time
 import os
-def cropping_fram(filename):
+def cropping_fram(filename,cnt):
     img = Image(filename)
+    imgcopy=img
     print("image is in crop function")
     #img = Image("testmark.jpg")
     #corners=[(0,0),(3030, -800),(3600,2044),(250,1944)]
@@ -17,12 +18,19 @@ def cropping_fram(filename):
     #print (countingcoordinates(blobs))
     img=findingblobs(img)
     if img == "false":
-        foo=0
-	print("oj")
-        #WHAT WILL HAPPEN IF BOARD IS BLOCKED
+        cnt=cnt+1
+        foo=1
+        print("blackboard not found, repeating! ")
+        time.sleep(1)
+        if (cnt <5):
+            #add the function that takes a new picture here
+            cropping_fram(filename,cnt)
+        else:
+            imgcopy.save(filename+"2.jpg")
+
     else:
-        img.save(filename)
-	print("image cropped and saved")
+        img.save(filename+"2.jpg")
+        print("image cropped and saved")
 #    img=os.system('raspistill -o '+"newpct.jpg")
 #    findingblobs(img)
 
@@ -31,8 +39,9 @@ def cropping_fram(filename):
     
 
 
-def cropping_bak(filename):
+def cropping_bak(filename,cnt):
     img = Image(filename)
+    imgcopy=img
     print("image is in crop function")
     #img = Image("testmark.jpg")
     corners=[(0,0),(2992, -900),(3500,2044),(250,1944)]
@@ -42,10 +51,19 @@ def cropping_bak(filename):
 
     img=findingblobsbak(img)
     if img == "false":
-        foo=0
-        #WHAT WILL HAPPEN IF BOARD IS BLOCKED
+        cnt=cnt+1
+        foo=1
+        print("blackboard not found, repeating! ")
+        time.sleep(1)
+        if (cnt <5):
+            #add the function that takes a new picture here
+            cropping_bak(filename,cnt)
+        else:
+            imgcopy.save(filename+"2.jpg")
+        #HOW TO CANCEL IF THE SENSOR IS ACTIVATED?? possible rebuild of the main program
+        #mainprog()
     else:
-        img.save(filename)
-
+        img.save(filename+"2.jpg")
+        print("image cropped and saved")
     
     #Rader som tar en ny bild efter en stunds sleep
